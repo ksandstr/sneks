ab65c1ca2598c1e373439ff9d6910a695acf06cd
Kalle A. Sandström <ksandstr@iki.fi>
2018-10-08 16:35:58 +0300

    add WIFEXITED() etc. macros to <sys/wait.h>
    There's some that aren't defined quite yet.

--- include/sys/wait.h
+++ include/sys/wait.h
@@ -9,6 +9,17 @@
 #include <sneks/process.h>
 
 
+#define WIFEXITED(st) (((st) & 1) != 0)
+#define WEXITSTATUS(st) ((st) >> 1)
+#define WIFSIGNALED(st) (((st) & 3) == 0)
+#define WTERMSIG(st) ((st) >> 2)
+#define WCOREDUMP(st) (((st) & 3) == 2)
+/* FIXME: fill these in once they can be tested */
+#define WIFSTOPPED(st) 0
+#define WSTOPSIG(st) 0
+#define WIFCONTINUED(st) 0
+
+
 extern pid_t wait(int *wstatus);
 extern pid_t waitpid(pid_t pid, int *wstatus, int options);
 extern int waitid(idtype_t idtype, id_t id, siginfo_t *infop, int options);
