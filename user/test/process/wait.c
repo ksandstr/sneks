d3d67e1338ca1611193c050af90fc14b89ea8b9a
Kalle A. Sandström <ksandstr@iki.fi>
2018-10-08 12:35:37 +0300

    user/test: test wait(2) when there are no children

---
+++ user/test/process/wait.c
@@ -0,0 +1,22 @@
+
+/* tests on the wait(2), waitpid(2), and waitid(2) syscalls. */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <sneks/test.h>
+
+
+START_TEST(empty_wait)
+{
+	plan_tests(2);
+	todo_start("WIP");
+
+	int st, pid = wait(&st);
+	ok1(pid == -1);
+	ok1(errno == ECHILD);
+}
+END_TEST
+
+DECLARE_TEST("process:wait", empty_wait);
