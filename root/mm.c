faa6b4bd3c2babb4d3d3635bec5ad0db9e870b1a
Kalle A. Sandström <ksandstr@iki.fi>
2018-10-19 19:29:05 +0300

    root: correct a sbrk() and sysmem interaction
    And call L4_Sigma0_GetPage() in a loop, since it may return whatever shrapnel
    fits in the specified range. This still expects sigma0 to return either what
    was requested or nothing before sysmem is up, so it's not robust against a
    willfully simplistic sigma0.

--- root/mm.c
+++ root/mm.c
@@ -32,6 +32,49 @@ static uintptr_t current_brk = 0, heap_bottom;
 static L4_Fpage_t pre_heap_pages[N_PRE_HEAP];
 static int pre_heap_pos = 0;	/* < 0 indicates sysmem paging */
 
+static L4_ThreadId_t sysmem_tid;
+
+
+static void get_more_memory(L4_Word_t start)
+{
+	/* get physical memory from sigma0 in case it's not been pumped yet. */
+	const bool have_sysmem = pre_heap_pos < 0;
+	int size_log2;
+	L4_Word_t address;
+	for_page_range(start, current_brk, address, size_log2) {
+		/* sigma0 returns a page within the range, which is the page we're
+		 * asking for iff there's no applicable shrapnel yet. so we should
+		 * keep asking until we either get the exact request, or nilpage
+		 * once the range has emptied.
+		 */
+		L4_Fpage_t p;
+		do {
+			p = L4_Sigma0_GetPage(L4_nilthread,
+				L4_FpageLog2(address, size_log2));
+			if(L4_IsNilFpage(p)) {
+				if(have_sysmem) break;
+				printf("s0 GetPage failed! ec=%lu\n", L4_ErrorCode());
+				abort();
+			}
+			if(have_sysmem) {
+				/* transfer immediately. */
+				extern L4_ThreadId_t sysmem_tid;	/* FIXME: terrible hack */
+				send_phys_to_sysmem(sysmem_tid, true, p);
+			} else {
+				/* defer until mm_enable_sysmem(). */
+				if(pre_heap_pos == N_PRE_HEAP) {
+					printf("%s: ran out of pre-heap pages!\n", __func__);
+					abort();
+				}
+				assert(pre_heap_pos < N_PRE_HEAP);
+				pre_heap_pages[pre_heap_pos++] = p;
+				/* we can require this in pristine pre-sysmem land. */
+				assert(L4_Address(p) == address && L4_SizeLog2(p) == size_log2);
+			}
+		} while(L4_Address(p) != address || L4_SizeLog2(p) != size_log2);
+	}
+}
+
 
 void *sbrk(intptr_t increment)
 {
@@ -45,28 +88,11 @@ void *sbrk(intptr_t increment)
 	void *ret = (void *)current_brk;
 	if(increment > 0) {
 		current_brk = (current_brk + increment + PAGE_MASK) & ~PAGE_MASK;
-		if(unlikely(pre_heap_pos >= 0)) {
-			/* pre-sysmem heap handling. */
-			int size_log2;
-			L4_Word_t address, start = (L4_Word_t)ret;
-			for_page_range(start, current_brk, address, size_log2) {
-				L4_Fpage_t p = L4_Sigma0_GetPage(L4_nilthread,
-					L4_FpageLog2(address, size_log2));
-				if(L4_IsNilFpage(p)) {
-					printf("s0 GetPage failed! ec=%lu\n", L4_ErrorCode());
-					abort();
-				}
-				assert(L4_Address(p) == address);
-				assert(L4_SizeLog2(p) == size_log2);
-				if(pre_heap_pos == N_PRE_HEAP) {
-					printf("%s: ran out of pre-heap pages!\n", __func__);
-					abort();
-				}
-				assert(pre_heap_pos < N_PRE_HEAP);
-				pre_heap_pages[pre_heap_pos++] = p;
-			}
-		}
+		get_more_memory((L4_Word_t)ret);
 	} else if(increment < 0) {
+		/* FIXME: before sysmem, get_more_memory() will break on pages it has
+		 * already taken but which it expects sigma0 to hand out again.
+		 */
 		increment = (-increment + PAGE_MASK) & ~PAGE_MASK;
 		current_brk -= increment;
 		if(current_brk < heap_bottom) current_brk = heap_bottom;
@@ -84,8 +110,9 @@ void *sbrk(intptr_t increment)
 }
 
 
-COLD void mm_enable_sysmem(L4_ThreadId_t sysmem_tid)
+COLD void mm_enable_sysmem(L4_ThreadId_t sm_tid)
 {
+	sysmem_tid = sm_tid;
 	/* send me your money! */
 	for(int i=0; i < pre_heap_pos; i++) {
 		send_phys_to_sysmem(sysmem_tid, true, pre_heap_pages[i]);
