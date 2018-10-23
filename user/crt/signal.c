bd9c43856d0dfebe6d43fcd24e43d506f48b81f5
Kalle A. Sandström <ksandstr@iki.fi>
2018-10-23 23:31:50 +0300

    user/crt: implement pause(2) and kill(2)

--- user/crt/signal.c
+++ user/crt/signal.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <assert.h>
 #include <errno.h>
+#include <ccan/likely/likely.h>
 
 #include <l4/types.h>
 #include <l4/ipc.h>
@@ -15,12 +16,35 @@
 
 int pause(void)
 {
-	return 0;
+	bool warned = false;
+	L4_MsgTag_t tag;
+	do {
+		/* sleep in a self-send phase that never ends to enable fast siginvoke. */
+		L4_LoadMR(0, 0);
+		tag = L4_Send(L4_Myself());
+		if((L4_IpcSucceeded(tag) || L4_ErrorCode() != 6) && !warned) {
+			fprintf(stderr, "%s: odd ErrorCode=%lu (or success at self-send?)\n",
+				__func__, L4_IpcFailed(tag) ? L4_ErrorCode() : 0);
+			warned = true;
+		}
+	} while(L4_IpcSucceeded(tag) || L4_ErrorCode() != 6);
+
+	errno = EINTR;
+	return -1;
 }
 
 
 int kill(int pid, int signum)
 {
-	errno = ENOSYS;
-	return -1;
+	int n = __proc_kill(__the_sysinfo->api.proc, pid, signum);
+	if(likely(n == 0)) return 0;
+	else if(n < 0) {
+		errno = -n;
+		return -1;
+	} else {
+		assert(n > 0);
+		fprintf(stderr, "%s: can't reach Sneks::Proc; n=%d\n", __func__, n);
+		errno = -ENOSYS;
+		return -1;
+	}
 }
