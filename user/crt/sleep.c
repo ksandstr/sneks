3ef57d22a1585d250375fb784b20167932e2b558
Kalle A. Sandström <ksandstr@iki.fi>
2019-05-03 23:37:29 +0300

    user/crt: sleep(2) in IPC send phase
    And same for usleep(2). Turns out that otherwise they'll inhibit signal
    response.

--- user/crt/sleep.c
+++ user/crt/sleep.c
@@ -11,29 +11,33 @@
 #include <l4/ipc.h>
 
 
+/* TODO: change these to receive-phase sleeps and make them explicitly
+ * interruptable.
+ */
 unsigned int sleep(unsigned int seconds)
 {
 	/* TODO: use a TimePoint instead once those become available. */
 	L4_Clock_t start = L4_SystemClock();
-	L4_MsgTag_t tag = L4_Receive_Timeout(L4_MyGlobalId(),
+	L4_MsgTag_t tag = L4_Send_Timeout(L4_MyGlobalId(),
 		L4_TimePeriod((uint64_t)seconds * 1000000));
 	assert(L4_IpcFailed(tag));
-	if(L4_ErrorCode() == 3) return 0;	/* receive phase timeout */
+	if(L4_ErrorCode() == 2) return 0;	/* send phase timeout */
 	else {
-		assert(L4_ErrorCode() == 6);	/* canceled in receive phase */
+		assert(L4_ErrorCode() == 6);	/* canceled in send phase */
 		L4_Clock_t end = L4_SystemClock();
 		return (end.raw - start.raw + 500000) / 1000000;
 	}
 }
 
 
+/* see TODO for sleep() */
 int usleep(__useconds_t usec)
 {
-	L4_MsgTag_t tag = L4_Receive_Timeout(L4_MyGlobalId(), L4_TimePeriod(usec));
+	L4_MsgTag_t tag = L4_Send_Timeout(L4_MyGlobalId(), L4_TimePeriod(usec));
 	assert(L4_IpcFailed(tag));
-	if(L4_ErrorCode() == 3) return 0;	/* receive phase timeout */
+	if(L4_ErrorCode() == 2) return 0;	/* send phase timeout */
 	else {
-		assert(L4_ErrorCode() == 6);	/* canceled in receive phase */
+		assert(L4_ErrorCode() == 6);	/* canceled in send phase */
 		errno = EINTR;
 		return -1;
 	}
