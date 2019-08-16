27a90566f013071ee90e9f53945aefbfaaf2554d
Kalle A. Sandström <ksandstr@iki.fi>
2019-08-16 21:14:35 +0300

    lib: restore name of hash32shiftmult()

--- include/sneks/hash.h
+++ include/sneks/hash.h
@@ -7,8 +7,12 @@
 #include <stdint.h>
 
 
-extern uint32_t int_hash(uint32_t key);
+extern uint32_t hash32shiftmult(uint32_t key);
+
+#ifndef SNEKS_LIB_HASH_IMPL
+#define int_hash(x) hash32shiftmult((x))
 #define word_hash(x) int_hash((uint32_t)(x))
+#endif
 
 extern uint32_t int64_hash(uint64_t key);
 extern uint32_t bob96bitmix(uint32_t a, uint32_t b, uint32_t c);
--- lib/hash.c
+++ lib/hash.c
@@ -3,27 +3,15 @@
 
 #include <stdint.h>
 
+#define SNEKS_LIB_HASH_IMPL 1
 #include <sneks/hash.h>
-
-
-/* hash32shiftmult(); presumed to have been in the public domain. */
-uint32_t int_hash(uint32_t key)
-{
-	uint32_t c2=0x27d4eb2du; // a prime or an odd constant
-	key = (key ^ 61) ^ (key >> 16);
-	key = key + (key << 3);
-	key = key ^ (key >> 4);
-	key = key * c2;
-	key = key ^ (key >> 15);
-	return key;
-}
 
 
 /* probably this is slower than need be. but it'll do, and it's two distinct
  * dep chains anyway so maybe not so much slower.
  */
 uint32_t int64_hash(uint64_t key) {
-	return int_hash(key) ^ int_hash(key >> 32);
+	return hash32shiftmult(key) ^ hash32shiftmult(key >> 32);
 }
 
 
@@ -42,4 +30,24 @@ uint32_t bob96bitmix(uint32_t a, uint32_t b, uint32_t c)
 	b=b-c;  b=b-a;  b=b^(a << 10);
 	c=c-a;  c=c-b;  c=c^(b >> 15);
 	return c;
+}
+
+
+/* via Bob and C-ized; presumed to have been in the public domain, continues
+ * to be if so.
+ */
+uint32_t hash32shiftmult(uint32_t key)
+{
+	uint32_t c2 = 0x27d4eb2du;	/* a prime or an odd constant */
+	key = (key ^ 61) ^ (key >> 16);
+	key = key + (key << 3);
+	key = key ^ (key >> 4);
+	key = key * c2;
+	key = key ^ (key >> 15);
+	return key;
+}
+
+
+uint32_t int_hash(uint32_t x) {
+	return hash32shiftmult(x);
 }
