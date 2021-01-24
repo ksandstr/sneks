c7b286e54e1fd0cb2d568ab71868423bb05d7d23
Kalle A. Sandström <ksandstr@iki.fi>
2021-01-24 12:17:04 +0200

    stuff: match suite:tcase patterns in get-test-id.pl
    This makes it possible to fetch test IDs for e.g. tests in io:pipe named
    `eof', when otherwise a simpler "pipe" query would yield poll/select/epoll
    tests only.

--- stuff/get-test-id.pl
+++ stuff/get-test-id.pl
@@ -24,13 +24,34 @@ $ENV{opts_name} = "--describe";
 $ENV{SYSTEST} = 1 if $test_system;
 open(DESCRIBE, "./run.sh -display none 2>&1 |")
 	|| die "can't open pipe from run.sh!";
-while(<DESCRIBE>) {
-	chomp;
+my ($suite, $tcase);
+LINE: while(my $line = <DESCRIBE>) {
+	chomp $line;
+	study $line;
+	if($line =~ /desc\s+suite\s+\`(\w+)'/) {
+		$suite = $1;
+		next LINE;
+	} elsif($line =~ /desc\s+tcase\s+\`(\w+)'/) {
+		$tcase = $1;
+		next LINE;
+	}
 	my $match = 0;	# of @ARGV patterns matched on this line.
 	for my $pat (@ARGV) {
-		/desc\s+test\s+\`(\w*$pat\w*)'\s+(.*)$/ or next;
-		my %p = map { /(\w+):(\w+)/; ($1, $2) } split /\s+/;
-		print "matched `$pat': $1 -> $p{id}";
+		my $test_name;
+		if(index($pat, ':') >= 0 && $suite && $tcase
+			&& $pat =~ /^(\w+):(\w+)$/)
+		{
+			# suite:tcase patterns
+			next unless $suite eq $1 && $tcase eq $2
+				&& $line =~ /desc\s+test\s+\`(\w+)'/;
+			$test_name = $1;
+		} else {
+			# patterns on the test name
+			$line =~ /desc\s+test\s+\`(\w*$pat\w*)'/ or next;
+			$test_name = $1;
+		}
+		my %p = map { /(\w+):(\w+)/ ? ($1, $2) : () } split(/\s+/, $line);
+		print "matched `$pat': $test_name -> $p{id}";
 		my $add = $p{id};
 		if(exists $p{low} && exists $p{high} && $p{high} > 0) {
 			print " [$p{low}..$p{high}]";
@@ -40,7 +61,9 @@ while(<DESCRIBE>) {
 		$match++;
 		push @matched, $add;
 	}
-	if(!$match && (/\*\*\*\s+begin\s+suite\s+/ || /\*\*\* .+ completed/)) {
+	if(!$match && ($line =~ /\*\*\*\s+begin\s+suite\s+/
+		|| $line =~ /\*\*\* .+ completed/))
+	{
 		# kill the piped child process and finish the loop. the "describe"
 		# output has ended so there'll be no further desc lines.
 		$SIG{TERM} = sub { };
