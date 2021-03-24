6c4a98047e12f7d8fc95de24967669193fd0bf01
Kalle A. Sandström <ksandstr@iki.fi>
2021-03-24 14:32:22 +0200

    user/test: enable filesystem access in hostsuite
    Now all test pathnames must be prefixed with TESTDIR, followed by whatever the
    Makefile target test-files-install uses after $(INITRD).

--- user/test/io/reg.c
+++ user/test/io/reg.c
@@ -12,10 +12,7 @@
 #include <sneks/test.h>
 
 
-/* TODO: make the test file available to hostsuite somehow */
-#ifdef __sneks__
-
-static char testfile_path[] = "/user/test/io/reg/testfile";
+static char testfile_path[] = TESTDIR "user/test/io/reg/testfile";
 static char expected_testfile_data[] = "0123456789abcdef";
 
 
@@ -127,5 +124,3 @@ START_TEST(seek_for_length)
 END_TEST
 
 DECLARE_TEST("io:reg", seek_for_length);
-
-#endif
