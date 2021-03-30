2d870ee1f8d18207aaa69445e225c59c96f0e193
Kalle A. Sandström <ksandstr@iki.fi>
2021-03-30 15:01:59 +0300

    user/test: make dup(2) family tests stronger
    ⑨

--- user/test/io/stdfile.c
+++ user/test/io/stdfile.c
@@ -31,7 +31,7 @@ DECLARE_TEST("io:stdfile", dup_and_close_stdin);
 
 START_TEST(fcntl_dupfd)
 {
-	plan_tests(4);
+	plan_tests(6);
 
 	int n = fcntl(1, F_DUPFD, 10);
 	if(!ok(n >= 10, "F_DUPFD(10)")) {
@@ -47,12 +47,16 @@ START_TEST(fcntl_dupfd)
 
 	n = fcntl(1, F_DUPFD, 679);
 	int n2 = fcntl(1, F_DUPFD, 678);
-	if(!ok(n >= 678 && n2 >= 678, "F_DUPFD(high), twice")) {
-		diag("n=%d, n2=%d, errno=%d", n, n2, errno);
+	int n3 = fcntl(1, F_DUPFD, 678);
+	if(!ok(n >= 678 && n2 >= 678 && n3 >= 678, "F_DUPFD(high), thrice")) {
+		diag("n=%d, n2=%d, n3=%d, errno=%d", n, n2, n3, errno);
 	}
 	imply_ok1(n >= 0 && n2 >= 0, n != n2);
+	imply_ok1(n >= 0 && n3 >= 0, n != n3);
+	imply_ok1(n2 >= 0 && n3 >= 0, n2 != n3);
 	if(n >= 0) close(n);
 	if(n2 >= 0) close(n2);
+	if(n3 >= 0) close(n3);
 }
 END_TEST
 
@@ -64,19 +68,27 @@ DECLARE_TEST("io:stdfile", fcntl_dupfd);
  * handles aren't duplicated but rather userspace must reference count its
  * descriptors.
  */
-START_LOOP_TEST(many_dups, iter, 0, 1)
+START_LOOP_TEST(many_dups, iter, 0, 3)
 {
 	const int n_dups = (iter & 1) ? 567 : 11;
-	diag("n_dups=%d", n_dups);
-	plan_tests(6);
+	const bool use_pipe = !!(iter & 2);
+	diag("n_dups=%d, use_pipe=%s", n_dups, btos(use_pipe));
+	plan_tests(8);
 
 	int n = sigaction(SIGPIPE,
 		&(struct sigaction){ .sa_handler = SIG_IGN }, NULL);
 	fail_unless(n == 0, "sigaction: errno=%d", errno);
 
-	int fds[2];
-	n = pipe(fds);
-	ok(n == 0, "pipe");
+	int fds[2] = { -1, -1 };
+	if(use_pipe) {
+		n = pipe(fds);
+		if(!ok(n == 0, "pipe(2)")) diag("errno=%d", errno);
+		skip(1, "use_pipe=%s", btos(use_pipe));
+	} else {
+		skip(1, "use_pipe=%s", btos(use_pipe));
+		fds[0] = open(TESTDIR "/user/test/io/reg/testfile", O_RDONLY);
+		if(!ok(fds[0] >= 0, "open(2)")) diag("errno=%d", errno);
+	}
 
 	bool dups_ok = true;
 	int *dups = malloc(n_dups * sizeof *dups);
@@ -103,17 +115,21 @@ START_LOOP_TEST(many_dups, iter, 0, 1)
 	}
 	ok(close_ok, "dups closed");
 
-	/* read side shouldn't yet have closed. */
-	n = write(fds[1], &(char){ 'x' }, 1);
-	ok(n == 1, "pipe not yet broken");
+	skip_start(!use_pipe, 1, "use_pipe=%s", btos(use_pipe)) {
+		/* read side shouldn't yet have closed. */
+		n = write(fds[1], &(char){ 'x' }, 1);
+		ok(n == 1, "pipe not yet broken");
+	} skip_end;
 
 	ok1(close(fds[0]) == 0);
 
-	/* verify that the read side has now closed. */
-	n = write(fds[1], &(char){ 'x' }, 1);
-	ok(n < 0 && errno == EPIPE, "pipe broken");
+	skip_start(!use_pipe, 2, "use_pipe=%s", btos(use_pipe)) {
+		/* verify that the read side has now closed. */
+		n = write(fds[1], &(char){ 'x' }, 1);
+		ok(n < 0 && errno == EPIPE, "pipe broken");
 
-	close(fds[1]);
+		ok1(close(fds[1]) == 0);
+	} skip_end;
 }
 END_TEST
 
