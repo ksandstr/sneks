d602b41d47eda4d86814f1e9d25ced274e680185
Kalle A. Sandström <ksandstr@iki.fi>
2021-05-07 11:09:04 +0300

    user/crt: support read, close, and seek in fdopen()

--- user/crt/stdio.c
+++ user/crt/stdio.c
@@ -1,6 +1,4 @@
 
-/* (no longer derived from fake_stdio.c of mung as of 20190516.) */
-
 #include <stdio.h>
 #include <stdint.h>
 #include <assert.h>
@@ -13,6 +11,16 @@
 #include "private.h"
 
 
+static long fd_read(void *cookie, char *buf, size_t size)
+{
+	int n;
+	do {
+		n = read((int)cookie, buf, size);
+	} while(n < 0 && errno == EINTR);
+	return max_t(long, n, 0);
+}
+
+
 static long fd_write(void *cookie, const char *buf, size_t size)
 {
 	int n;
@@ -23,10 +31,22 @@ static long fd_write(void *cookie, const char *buf, size_t size)
 }
 
 
+static int fd_seek(void *cookie, off64_t *offset, int whence) {
+	off_t n = lseek((int)cookie, *offset, whence);
+	if(n >= 0) *offset = n;
+	return (int)n;
+}
+
+
+static int fd_close(void *cookie) {
+	return 0;
+}
+
+
 FILE *fdopen(int fd, const char *mode)
 {
 	return fopencookie((void *)fd, mode, (cookie_io_functions_t){
-		.write = &fd_write,
-		/* TODO: add read, close */
+		.write = &fd_write, .read = &fd_read,
+		.close = &fd_close, .seek = &fd_seek,
 	});
 }
