6e4517ba88f7cc44a67c71248670ba39b627442b
Kalle A. Sandström <ksandstr@iki.fi>
2021-07-23 17:26:31 +0300

    user/test: add looped symlink test to io:symlink
    Passes hostcheck, todo for sneks.

--- user/test/io/symlink.c
+++ user/test/io/symlink.c
@@ -9,10 +9,16 @@
  */
 
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <alloca.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <ccan/str/str.h>
+#include <ccan/array_size/array_size.h>
 
 #include <sneks/test.h>
 
@@ -66,3 +72,39 @@ START_LOOP_TEST(readlink, iter, 0, 2)
 END_TEST
 
 DECLARE_TEST("io:symlink", readlink);
+
+
+/* test symlink dereferencing in the case where a symlink leads to a
+ * neverending loop, either as a terminal element or a non-terminal one.
+ */
+START_TEST(deref_loop)
+{
+	static const char *const cases[] = {
+		TESTDIR "/user/test/io/symlink/looping_terminal",
+		TESTDIR "/user/test/io/symlink/looping_middle/whatever/else",
+	};
+	plan_tests(ARRAY_SIZE(cases));
+
+#ifdef __sneks__
+	todo_start("unimplemented");
+#endif
+
+	for(int i=0; i < ARRAY_SIZE(cases); i++) {
+		char *namepart = strstr(cases[i], "looping_"), *slash = strchr(namepart, '/');
+		if(slash != NULL) {
+			int len = slash - namepart;
+			assert(namepart[len] == '/');
+			char *copy = alloca(len + 1);
+			memcpy(copy, namepart, len);
+			copy[len] = '\0';
+			namepart = copy;
+		}
+		errno = 0;
+		if(!ok(stat(cases[i], &(struct stat){ }) < 0 && errno == ELOOP, "ELOOP for %s", namepart)) {
+			diag("errno=%d", errno);
+		}
+	}
+}
+END_TEST
+
+DECLARE_TEST("io:symlink", deref_loop);
