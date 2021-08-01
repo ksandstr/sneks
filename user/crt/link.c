f522594da315c6d455c57d4593d33f6141cd71da
Kalle A. Sandström <ksandstr@iki.fi>
2021-08-02 01:20:28 +0300

    user/crt: change link.c to use PATH_MAX from Sneks::Path
    The caller's PATH_MAX should be different, so we follow the IDL spec instead.
    That's obviously correct.

--- user/crt/link.c
+++ user/crt/link.c
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <ccan/minmax/minmax.h>
 
+#include <sneks/api/path-defs.h>
 #include <sneks/api/directory-defs.h>
 
 #include "private.h"
@@ -27,13 +28,14 @@ ssize_t readlinkat(int dirfd, const char *restrict path,
 	if(n != 0) return NTOERR(n);
 	if(r.ifmt >> 12 != SNEKS_DIRECTORY_DT_LNK) goto Einval;
 
-	char *pathbuf = bufsize < PATH_MAX ? alloca(PATH_MAX + 1) : buf;
+	const size_t path_max = SNEKS_PATH_PATH_MAX;
+	char *pathbuf = bufsize < path_max ? alloca(path_max + 1) : buf;
 	int pathlen = 0;
 	n = __dir_readlink(r.server, pathbuf, &pathlen, r.object, r.cookie);
 	if(n != 0) return NTOERR(n);
 	assert(pathbuf[pathlen] == '\0');
 
-	if(bufsize < PATH_MAX) memcpy(buf, pathbuf, min_t(size_t, pathlen, bufsize));
+	if(bufsize < path_max) memcpy(buf, pathbuf, min_t(size_t, pathlen, bufsize));
 	if(pathlen < bufsize) buf[pathlen] = '\0';	/* mr. nice guy */
 	return min_t(size_t, pathlen, bufsize);
 
