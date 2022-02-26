a580151bcc373ddbb9c8e913a0b927526c733a4d
Kalle A. Sandström <ksandstr@iki.fi>
2022-02-26 21:27:29 +0200

    root: clean up and recomment random.c
    It should have a proper algorithm cloned from elsewhere. Right now it doesn't;
    for example, XTEA is treated as a black-box algorithm that would somehow have
    good distribution properties wrt bits coming from the entropy pool.
    
    What it does do is demonstrate useful entropy collection from sneks booting
    under qemu and/or KVM. How much and to what overall effect isn't known.

--- root/random.c
+++ root/random.c
@@ -1,18 +1,14 @@
-
 /* random number generation using the XTEA algorithm from CCAN in some kind of
  * a quarter-arsed CTR mode. it's likely that XTEA is way too powerful and/or
  * expensive for this kind of thing; and definitely the case that hardware AES
  * should be used instead where available.
  */
-
 #include <stdint.h>
 #include <string.h>
 #include <limits.h>
 #include <ccan/minmax/minmax.h>
 #include <ccan/crypto/xtea/xtea.h>
-
 #include "defs.h"
-
 
 static struct xtea_secret xstate;
 static uint64_t xplaintext = 0x1023456789abcdefull;
@@ -28,13 +24,11 @@ static int buffer_depth = 0;
 static uint64_t epool = 0xfedcba9876543210ull;
 static int epool_depth = 0;
 
-
 /* this can be called any number of times. */
 void random_init(uint64_t x) {
 	xplaintext ^= x;
 	xplaintext = (xplaintext << 63) | (xplaintext >> 1);
 }
-
 
 void add_entropy(uint64_t value)
 {
@@ -44,16 +38,17 @@ void add_entropy(uint64_t value)
 	if(value != 0 && ++epool_depth > 64) epool_depth = 64;
 }
 
-
 /* "/dev/urandom" style output. not guaranteed prediction resistant, so only
- * crypto strength for short sequences and when good entropy was available.
+ * full strength for short sequences and when good entropy was available.
+ * this algorithm was invented at random as a substitute for a proper thing
+ * that looks right under test, so it should be replaced with something real
+ * by the time sneks gets any access control at all.
  */
 void generate_u(void *outbuf, size_t length)
 {
 	int budget = 4;	/* don't drain the pool entirely */
 	size_t done = 0;
 	while(done < length) {
-		int n;
 		if(buffer_depth == 0) {
 			if(budget > 0 && epool_depth > 0) {
 				uint8_t *sp = &xstate.u.u8[perturb_pos++ & 15];
@@ -66,7 +61,7 @@ void generate_u(void *outbuf, size_t length)
 			buffer.u64 = xtea_encipher(&xstate, xplaintext++);
 			buffer_depth = 8;
 		}
-		n = min_t(int, length - done, buffer_depth);
+		int n = min_t(int, length - done, buffer_depth);
 		memcpy(outbuf + done, &buffer.u8[8 - buffer_depth], n);
 		buffer_depth -= n;
 		done += n;
