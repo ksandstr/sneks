07d530dee47113efd4d30c50e00132e8b9de70eb
Kalle A. Sandström <ksandstr@iki.fi>
2022-05-03 11:53:46 +0300

    lib: refresh <sneks/bitops.h>

--- include/sneks/bitops.h
+++ include/sneks/bitops.h
@@ -1,25 +1,23 @@
-
 #ifndef __SNEKS_BITOPS_H__
 #define __SNEKS_BITOPS_H__
 
-#include <stdlib.h>
-#include <stdint.h>
+#include <limits.h>
+#include <stddef.h>
 
-
-/* ia32: returns 31..0 */
-#define MSB(x) (sizeof(unsigned long) * 8 - __builtin_clzl((unsigned long)(x)) - 1)
-
-/* ia32: returns 63..0 */
-#define MSBLL(x) (sizeof(unsigned long long) * 8 \
-	- __builtin_clzll((unsigned long long)(x)) - 1)
+/* returns LONG_BIT - 1 .. 0 */
+#define MSBL(x) (LONG_BIT - __builtin_clzl((unsigned long)(x)) - 1)
+/* returns ULLONG_BIT - 1 .. 0 */
+#define MSBLL(x) (sizeof(unsigned long long) * 8 - __builtin_clzll((unsigned long long)(x)) - 1)
+/* returns WORD_BIT - 1 .. 0 */
+#define MSB(x) MSBL((int)(x))
 
 static inline int size_to_shift(size_t sz) {
-	int msb = MSB(sz);
+	int msb = MSBL(sz);
 	return (1 << msb) < sz ? msb + 1 : msb;
 }
 
-/* 32-bit comparison-to-mask conversion. zero -> 0, nonzero -> ~0. */
-#define MASK32(b) ((int32_t)((b) | -(b)) >> 31)
-
+/* zero -> 0, nonzero -> ~0. */
+#define SIGNED_MASK(b) ((long)((b) | -(b)) >> (LONG_BIT - 1))
+#define MASK32(b) SIGNED_MASK((int)(b))
 
 #endif
--- lib/string.c
+++ lib/string.c
@@ -9,6 +9,7 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 #include <limits.h>
