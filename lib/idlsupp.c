393a737d249b4ab763552688440f0fb6dac14473
Kalle A. Sandström <ksandstr@iki.fi>
2022-05-31 15:51:34 +0300

    lib: refactor idlsupp.c

--- lib/idlsupp.c
+++ lib/idlsupp.c
@@ -1,51 +1,25 @@
-/* support bits for µIDL.
- *
- * TODO: this interface isn't obviously defined anywhere, so its future
- * consistency is questionable.
- */
-
-#include <stdio.h>
+/* support bits for µIDL. such as they are. */
 #include <stdlib.h>
-#include <string.h>
-#include <assert.h>
 #include <threads.h>
-
 
 static tss_t key;
 static once_flag init_once = ONCE_FLAG_INIT;
 
-
-static void create_key(void)
-{
-	int n = tss_create(&key, &free);
-	if(n != thrd_success) {
-		fprintf(stderr, "%s: can't create muidl support tss_t, n=%d\n",
-			__func__, n);
-		abort();
-	}
+static void create_key(void) {
+	if(tss_create(&key, &free) != thrd_success) abort();
 }
-
 
 void muidl_supp_alloc_context(unsigned int length)
 {
 	call_once(&init_once, &create_key);
-
 	void *ctx = tss_get(key);
 	if(ctx != NULL) free(ctx);
-
 	if(length < 64) length = 64;
-	ctx = malloc(length);
-	if(ctx == NULL) {
-		printf("%s: malloc(%u) failed\n", __func__, length);
-		abort();
-	}
-	memset(ctx, '\0', length);
+	if(ctx = calloc(1, length), ctx == NULL) abort();
 	tss_set(key, ctx);
 }
 
-
-void *muidl_supp_get_context(void)
-{
+void *muidl_supp_get_context(void) {
 	call_once(&init_once, &create_key);
 	return tss_get(key);
 }
