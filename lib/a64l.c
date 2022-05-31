64196df90408d880f4469d6a2f9c6f3cbe15a5db
Kalle A. Sandström <ksandstr@iki.fi>
2022-05-31 15:37:05 +0300

    lib: refactor a64l.c

--- lib/a64l.c
+++ lib/a64l.c
@@ -1,4 +1,3 @@
-
 /* POSIX long to radix64 alphanumeric conversion (and back again). note that
  * these don't concatenate like an arbitrary-length base64 encoder would do;
  * the 6 bytes output actually encode 36 bits, so there'd be a 4-bit gap in
@@ -12,13 +11,10 @@
  * the first place. what we'll do instead is take the first 32 bits unsigned
  * from @value and encode that, and sign-extend the result in a64l().
  */
-
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <assert.h>
 #include <ccan/array_size/array_size.h>
-
 
 static uint8_t dec_table[] = {
 	['.'] = 0, ['/'] = 1,
@@ -35,45 +31,26 @@ static uint8_t dec_table[] = {
 	['y'] = 62, ['z'] = 63,
 };
 
-
-static int decode_byte(unsigned c, bool *fail)
-{
-	if(c >= ARRAY_SIZE(dec_table) || (dec_table[c] == 0 && c != '.')) {
-		*fail = true;
-		return -1;
-	} else {
-		return dec_table[c];
-	}
+static int decode_byte(unsigned c, bool *fail) {
+	if(c < ARRAY_SIZE(dec_table) && (dec_table[c] > 0 || c == '.')) return dec_table[c];
+	else { *fail = true; return -1; }
 }
 
-
-static int encode_byte(int b)
-{
-	static const char enc_table[64] =
-		"./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	if(b >= 0 && b < ARRAY_SIZE(enc_table)) return enc_table[b];
-	else return '&';	/* , you clown */
+static int encode_byte(int b) {
+	static const char enc_table[64] = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	if(b >= 0 && b < ARRAY_SIZE(enc_table)) return enc_table[b]; else return '&'; /* (non-posix error indicator) */
 }
 
-
-long a64l(const char *str64)
-{
-	int32_t acc = 0;
+long a64l(const char *str64) {
+	long acc = 0;
 	bool fail = false;
-	for(int i = 0, scale = 0; i < 6 && str64[i] != '\0'; i++, scale += 6) {
-		acc |= decode_byte(str64[i], &fail) << scale;
-	}
+	for(int i = 0, scale = 0; i < 6 && str64[i] != '\0' && !fail; i++, scale += 6) acc |= decode_byte(str64[i], &fail) << scale;
 	return fail ? 0 : acc;
 }
 
-
-char *l64a(long input)
-{
-	static char str[7];
-	uint32_t v = input;
-	for(int i=0; i < 6 && v > 0; i++, v >>= 6) {
-		str[i] = encode_byte(v & 0x3f);
-		str[i+1] = '\0';
-	}
+char *l64a(long input) {
+	static char str[7]; /* TODO: return a lib-wide TSS tempstr */
+	unsigned v = input;
+	for(int i = 0; i < 6 && v > 0; i++, v >>= 6) { str[i] = encode_byte(v & 0x3f); str[i + 1] = '\0'; }
 	return str;
 }
