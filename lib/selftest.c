a4f3639aa3395841f0758c952483bf47c37f9f38
Kalle A. Sandström <ksandstr@iki.fi>
2022-05-31 16:11:11 +0300

    lib: refactor selftest.c

--- lib/selftest.c
+++ lib/selftest.c
@@ -3,29 +3,22 @@
 #include <stdbool.h>
 #include <string.h>
 #include <errno.h>
+#include <ccan/array_size/array_size.h>
 #include <ccan/autodata/autodata.h>
 #include <ccan/str/str.h>
-
 #include <l4/types.h>
 #include <l4/ipc.h>
 #include <l4/kdebug.h>
-
 #include <sneks/process.h>
 #include <sneks/systask.h>
-
 #include "muidl.h"
 
-
-struct wrapctx
-{
+struct wrapctx {
 	L4_ThreadId_t caller, test;
 	FILE *oldout, *olderr;
 };
 
-
-static void load_spec(
-	int *offset, int *mrpos,
-	const char *name, L4_Word_t meta)
+static void load_spec(int *offset, int *mrpos, const char *name, L4_Word_t meta)
 {
 	if(--*offset < 0) {
 		L4_LoadMRs(*mrpos, 5, (L4_Word_t *)name);
@@ -34,42 +27,35 @@ static void load_spec(
 	}
 }
 
-
 static void describe_selftests(L4_ThreadId_t sender, int offset)
 {
 	size_t n_specs = 0;
 	struct utest_spec **specs = autodata_get(all_systask_selftests, &n_specs);
-
 	int mrpos = 1;
 	char prefix[21] = "", group[21] = "";
 	for(size_t i=0; i < n_specs && mrpos < 58; i++) {
 		/* NOTE: this doesn't handle empties well, i.e. "foo:" and ":bar".
 		 * don't use those.
 		 */
-		char *path = strdup(specs[i]->path), *sep = strchr(path, ':');
+		char *path = strdupa(specs[i]->path), *sep = strchr(path, ':');
 		if(sep != NULL) *sep = '\0';
-		if(strcmp(prefix, path) != 0) {
-			strncpy(prefix, path, sizeof prefix - 1); prefix[20] = '\0';
+		if(!streq(prefix, path)) {
+			strscpy(prefix, path, sizeof prefix);
 			load_spec(&offset, &mrpos, prefix, 1);
 		}
-		if(sep != NULL && strcmp(group, sep + 1) != 0) {
-			strncpy(group, sep + 1, sizeof group - 1); group[20] = '\0';
+		if(sep != NULL && !streq(group, sep + 1)) {
+			strscpy(group, sep + 1, sizeof group);
 			load_spec(&offset, &mrpos, group, 2);
 		}
 		/* (priority is 0xff at bitpos 26) */
 		load_spec(&offset, &mrpos, specs[i]->test->name,
-			(specs[i]->test->iter_low & 0xff) << 2
-			| (specs[i]->test->iter_high & 0xffff) << 10);
-		free(path);
+			(specs[i]->test->iter_low & 0xff) << 2 | (specs[i]->test->iter_high & 0xffff) << 10);
 	}
-
 	L4_LoadMR(0, (L4_MsgTag_t){ .X.u = mrpos }.raw);
 	L4_MsgTag_t tag = L4_Reply(sender);
-	if(L4_IpcFailed(tag)) {
-		printf("%s: reply failed, ec=%lu\n", __func__, L4_ErrorCode());
-	}
+	if(L4_IpcFailed(tag)) fprintf(stderr, "%s: reply failed: %s\n", __func__, stripcerr(L4_ErrorCode()));
+	autodata_free(specs);
 }
-
 
 static ssize_t wrap_write(void *cookie, const char *buf, size_t size)
 {
@@ -82,12 +68,11 @@ static ssize_t wrap_write(void *cookie, const char *buf, size_t size)
 		 */
 		return fwrite(buf, sizeof *buf, size, ctx->oldout);
 	}
-
 	/* output through the TAP conduit. */
 	L4_Accept(L4_UntypedWordsAcceptor);
 	L4_StringItem_t si = L4_StringItem(size, (void *)buf);
 	L4_LoadMR(0, (L4_MsgTag_t){ .X.u = 1, .X.t = 2 , .X.label = 0 }.raw);
-	L4_LoadMR(1, 1);	/* (fd#: 1 for out, 2 for err) */
+	L4_LoadMR(1, 1); /* (fd#: 1 for out, 2 for err) */
 	L4_LoadMRs(2, 2, si.raw);
 	L4_MsgTag_t tag = L4_Call(ctx->caller);
 	if(L4_IpcFailed(tag)) {
@@ -95,10 +80,8 @@ static ssize_t wrap_write(void *cookie, const char *buf, size_t size)
 		errno = EIO;
 		return -1;
 	}
-
 	return 0;
 }
-
 
 static int wrap_close(void *cookie)
 {
@@ -112,7 +95,6 @@ static int wrap_close(void *cookie)
 	return 0;
 }
 
-
 /* finds first test whose name is a prefix of @name. */
 static const struct utest_spec *find_test(const char *name)
 {
@@ -124,7 +106,6 @@ static const struct utest_spec *find_test(const char *name)
 	return NULL;
 }
 
-
 static void run_selftest(L4_ThreadId_t sender, int iter, const char *name)
 {
 	const struct utest_spec *t = find_test(name);
@@ -132,56 +113,35 @@ static void run_selftest(L4_ThreadId_t sender, int iter, const char *name)
 		plan(SKIP_ALL, "test `%s' not found!", name);
 		return;
 	}
-
 	/* wrap stdio in calls to @sender */
-	struct wrapctx ctx = {
-		.caller = sender, .test = L4_Myself(),
-		.oldout = stdout, .olderr = stderr,
-	};
-	FILE *wrapout = fopencookie(&ctx, "wb", (cookie_io_functions_t){
-		.write = &wrap_write, .close = &wrap_close });
+	struct wrapctx ctx = { .caller = sender, .test = L4_Myself(), .oldout = stdout, .olderr = stderr };
+	FILE *wrapout = fopencookie(&ctx, "wb", (cookie_io_functions_t){ .write = &wrap_write, .close = &wrap_close });
 	if(wrapout == NULL) {
-		fprintf(stderr, "%s: fopencookie() failed, errno=%d",
-			__func__, errno);
-		/* pop error up to partner? (it's just malloc failure though.) */
+		fprintf(stderr, "%s: fopencookie() failed: %s\n", __func__, strerror(errno));
 		return;
 	}
-
-	/* let's a-go */
-	L4_LoadMR(0, 0);
-	L4_Reply(sender);
-
+	L4_LoadMR(0, 0); L4_Reply(sender);
 	FILE *oldin = stdin;
 	stdin = stdout = stderr = wrapout;
-
 	tap_reset();
 	(*t->test->test_fn)(iter);
-
 	stdin = oldin; stdout = ctx.oldout; stderr = ctx.olderr;
 	fclose(wrapout);
 }
-
 
 bool selftest_handling(L4_Word_t status)
 {
 	L4_ThreadId_t sender = muidl_get_sender();
 	if(pidof_NP(sender) < SNEKS_MIN_SYSID) return false;
-
 	L4_MsgTag_t tag = muidl_get_tag();
+	L4_Word_t iter, offset, name[L4_UntypedWords(tag)];
 	switch(L4_Label(tag)) {
 		default: return false;
-		case 0xeffe: {
-			L4_Word_t offset; L4_StoreMR(1, &offset);
-			describe_selftests(sender, offset);
-			return true;
-		}
-		case 0xeffd: {
-			L4_Word_t iter; L4_StoreMR(1, &iter);
-			L4_Word_t name[L4_UntypedWords(tag)];
-			L4_StoreMRs(2, L4_UntypedWords(tag) - 1, name);
-			name[sizeof name / sizeof name[0] - 1] = 0;
+		case 0xeffe: L4_StoreMR(1, &offset); describe_selftests(sender, offset); return true;
+		case 0xeffd:
+			L4_StoreMR(1, &iter); L4_StoreMRs(2, L4_UntypedWords(tag) - 1, name);
+			name[ARRAY_SIZE(name) - 1] = 0;
 			run_selftest(sender, iter, (const char *)name);
 			return true;
-		}
 	}
 }
