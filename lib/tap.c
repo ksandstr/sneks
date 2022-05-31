2960c97429cf17527f326140db0e2ee56b2586d9
Kalle A. Sandström <ksandstr@iki.fi>
2022-05-31 23:28:09 +0300

    lib: refactor tap.c
    This removes nearly all of the old fugly and substitutes with more recent,
    contemporary ugly.

--- lib/tap.c
+++ lib/tap.c
@@ -1,7 +1,5 @@
-
-/* libtap-like interface. basically a rewritten version of libtap's tap.c,
+/* libtap-like interface. a thoroughly rewritten version of libtap's tap.c,
  * which carries the following license statement:
- *
  *-
  * Copyright (c) 2004 Nik Clayton
  * All rights reserved.
@@ -27,13 +25,8 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *-
- *
  * the aggregate work is hereby licensed with identical terms to the ones
  * above, excepting the copyright statement.
- */
-
-/* FIXME: replace calls to abort() to something that returns control to the
- * test harness (i.e. main()) thread.
  */
 
 #if !defined(__KERNEL__) || defined(ENABLE_SELFTEST)
@@ -46,236 +39,128 @@
 #include <stdarg.h>
 #include <assert.h>
 #include <ccan/likely/likely.h>
-
 #include <sneks/test.h>
 
-
-#define TRY_INIT do { \
-		if(unlikely(!tap_init_done)) { \
-			tap_reset(); \
-			tap_init_done = true; \
-		} \
-	} while(0)
-
-
-struct saved_ctx
-{
-	struct saved_ctx *next;
-	int depth;
-
+struct tapctx {
+	struct tapctx *succ;
 	bool no_plan, have_plan, skip_all, todo, test_died, no_plan_closed;
-	int num_tests_run, expected_tests, failed_tests;
-	char *todo_msg;
-
-	char testmsg[];
+	int depth, num_tests_run, expected_tests, failed_tests;
+	char *todo_msg, testmsg[];
 };
 
-
-static bool no_plan, have_plan, skip_all, todo, test_died, no_plan_closed;
-static int num_tests_run, expected_tests, failed_tests;
-static char *todo_msg = NULL;		/* must be valid on first reset */
-static struct saved_ctx *save_stack = NULL;		/* for subtests */
-
-static bool tap_init_done = false;
-
-
-static void reset_state(void)
-{
-	/* (yeah, I know) */
-	no_plan = have_plan = skip_all = todo = test_died = false;
-	no_plan_closed = false;
-	num_tests_run = expected_tests = failed_tests = 0;
-}
-
+static struct tapctx firstctx = { }, *tap = &firstctx;
 
 void tap_reset(void)
 {
-	reset_state();
-	while(save_stack != NULL) {
-		struct saved_ctx *next = save_stack->next;
-		free(save_stack->todo_msg);
-		free(save_stack);
-		save_stack = next;
+	while(tap != &firstctx) {
+		struct tapctx *next = tap->succ;
+		free(tap->todo_msg); free(tap);
+		tap = next;
 	}
-
-	free(todo_msg);
-	todo_msg = NULL;
+	free(tap->todo_msg);
+	*tap = (struct tapctx){ };
 }
-
 
 void subtest_start(const char *fmt, ...)
 {
-	va_list al;
-	va_start(al, fmt);
+	va_list al; va_start(al, fmt);
 	int msglen = vsnprintf(NULL, 0, fmt, al) + 1;
 	va_end(al);
-
-	struct saved_ctx *c = malloc(sizeof(*c) + msglen);
-	va_start(al, fmt);
-	vsnprintf(c->testmsg, msglen, fmt, al);
-	va_end(al);
-
-	c->depth = save_stack == NULL ? 1 : save_stack->depth + 1;
-	c->no_plan = no_plan;
-	c->have_plan = have_plan;
-	c->skip_all = skip_all;
-	c->todo = todo;
-	c->test_died = test_died;
-	c->num_tests_run = num_tests_run;
-	c->expected_tests = expected_tests;
-	c->failed_tests = failed_tests;
-	c->todo_msg = todo_msg;
-	c->no_plan_closed = no_plan_closed;
-	todo_msg = c->todo_msg != NULL ? strdup(c->todo_msg) : NULL;
-
-	c->next = save_stack;
-	save_stack = c;
-
-	reset_state();
+	struct tapctx *c = malloc(sizeof *c + msglen); if(c == NULL) abort();
+	*c = (struct tapctx){ .succ = tap, .depth = tap->depth + 1 };
+	if(tap->todo_msg != NULL) c->todo_msg = strdup(tap->todo_msg);
+	va_start(al, fmt); vsnprintf(c->testmsg, msglen, fmt, al); va_end(al);
+	tap = c;
 }
-
 
 char *subtest_pop(int *rc_p, void **freeptr_p)
 {
-	if(save_stack == NULL) {
-		diag("%s: save_stack == NULL", __func__);
-		abort();
-	}
-
+	if(tap == &firstctx) { diag("tried to pop no subtest?"); abort(); }
 	close_no_plan();
-	int exst = exit_status();
-	struct saved_ctx *c = save_stack;
-	save_stack = c->next;
-
-	/* (there's likely a nicer way to do this, but, meh.) */
-	no_plan = c->no_plan;
-	have_plan = c->have_plan;
-	skip_all = c->skip_all;
-	todo = c->todo;
-	test_died = c->test_died;
-	num_tests_run = c->num_tests_run;
-	expected_tests = c->expected_tests;
-	failed_tests = c->failed_tests;
-	no_plan_closed = c->no_plan_closed;
-	free(todo_msg); todo_msg = c->todo_msg;
-
-	if(exst != 0) diag("subtest exit_status=%d", exst);
-	if(rc_p != NULL) *rc_p = exst;
-	if(freeptr_p != NULL) *freeptr_p = c;
-	return c->testmsg;
+	int st = exit_status();
+	if(st != 0) diag("subtest exit_status=%d", st);
+	if(rc_p != NULL) *rc_p = st;
+	struct tapctx *dead = tap; tap = dead->succ;
+	free(dead->todo_msg);
+	if(freeptr_p != NULL) {
+		*freeptr_p = dead; return dead->testmsg;
+	} else {
+		free(dead); return NULL;
+	}
 }
-
 
 int subtest_end(void)
 {
-	int rc;
-	void *freeptr;
+	int rc; void *freeptr;
 	char *msg = subtest_pop(&rc, &freeptr);
-	/* "rc" is value of exit_status(), 0 when all tests passed. */
 	ok(rc == 0, "%s", msg);
 	free(freeptr);
 	return rc;
 }
 
-
-static void print_sub_prefix(void)
-{
-	if(save_stack != NULL) {
-		for(int i=0; i < save_stack->depth; i++) printf("    ");
-	}
+static void print_sub_prefix(void) {
+	for(int i=0; i < tap->depth; i++) printf("    ");
 }
-
 
 noreturn void vbail(const char *fmt, va_list args)
 {
 	fflush(stdout);
-	/* NOTE: bailouts don't get a subtest prefix. (maybe they ought to? at
-	 * least for forked subtests?)
+	/* no subtest prefix for bailouts; they fail the entire test.
+	 * (this should be different for forked subtests which don't.)
 	 */
 	printf("Bail out!  "); vfprintf(stdout, fmt, args); printf("\n");
 	fflush(stdout);
 	exit(255);
 }
 
-
-noreturn void bail(const char *fmt, ...)
-{
-	va_list al;
-	va_start(al, fmt);
-	vbail(fmt, al);
-	/* look mum, no hands */
-}
-
+noreturn void bail(const char *fmt, ...) { va_list al; va_start(al, fmt); vbail(fmt, al); }
 
 #ifndef __KERNEL__
-void _fail_unless(
-	int result,
-	const char *file, int line, const char *expr,
-	const char *fmt, ...)
+void _fail_unless(int result, const char *file, int line, const char *expr, const char *fmt, ...)
 {
-	if(unlikely(!result)) {
-		if(fmt == NULL) {
-			printf("Bail out!  `%s' failed in %s:%d\n", expr, file, line);
-		} else {
-			va_list ap;
-			va_start(ap, fmt);
-			char buf[512];
-			vsnprintf(buf, sizeof buf, fmt, ap);
-			va_end(ap);
-			printf("Bail out!  %s: `%s' in %s:%d\n", buf, expr, file, line);
-		}
-
-		/* fail_if() and fail_unless() have assert nature, so abort() it is. */
-		abort();
+	if(likely(result)) return;
+	else if(fmt == NULL) {
+		printf("Bail out!  `%s' failed in %s:%d\n", expr, file, line);
+	} else {
+		va_list ap; va_start(ap, fmt);
+		char buf[200]; vsnprintf(buf, sizeof buf, fmt, ap);
+		va_end(ap);
+		printf("Bail out!  %s: `%s' in %s:%d\n", buf, expr, file, line);
 	}
+	abort(); /* fail_if() and fail_unless() have assert nature, so abort() it is. */
 }
 #endif
 
-
-int _gen_result(
-	bool ok,
-	const char *func,
-	const char *file,
-	unsigned int line,
-	const char *test_name_fmt,
-	...)
+int _gen_result(bool ok, const char *func, const char *file, unsigned int line, const char *test_name_fmt, ...)
 {
-	TRY_INIT;
-	num_tests_run++;
-
-	char name_buf[512];	/* that's six lines of 80 bytes each. */
-	const char *test_name = NULL;
+	tap->num_tests_run++;
+	char *test_name = NULL, name_buf[200];
 	if(test_name_fmt != NULL) {
-		va_list al;
-		va_start(al, test_name_fmt);
-		vsnprintf(name_buf, sizeof(name_buf), test_name_fmt, al);
+		va_list al; va_start(al, test_name_fmt);
+		vsnprintf(name_buf, sizeof name_buf, test_name_fmt, al);
 		va_end(al);
 		test_name = name_buf;
-
 		bool found_ok = false;
 		for(int i=0; test_name[i] != '\0'; i++) {
 			char c = test_name[i];
-			if((c < '0' || c > '9') && c != ' ' && c != '\t'
-				&& c != '\n' && c != '\r')
-			{
+			if((c < '0' || c > '9') && c != ' ' && c != '\t' && c != '\n' && c != '\r') {
 				found_ok = true;
 				break;
 			}
 		}
 		if(!found_ok) {
-			diag("    You named your test `%s'. You shouldn't use numbers for your test names.",
-				test_name);
+			diag("    You named your test `%s'. You shouldn't use numbers for your test names.", test_name);
 			diag("    Very confusing.");
 		}
 	}
-
+	/* core */
 	print_sub_prefix();
 	if(!ok) {
 		printf("not ");
-		failed_tests++;
+		tap->failed_tests++;
 	}
-	printf("ok %d", num_tests_run);
-
+	printf("ok %d", tap->num_tests_run);
+	/* extension */
 	if(test_name != NULL) {
 		int len = strlen(test_name);
 		char escbuf[len * 2 + 1];
@@ -289,49 +174,33 @@ int _gen_result(
 		}
 		printf(" - %s", escbuf);
 	}
-
-	if(todo) {
-		printf(" # TODO %s", todo_msg);
-		if(!ok) failed_tests--;
+	/* adornment */
+	if(tap->todo) {
+		printf(" # TODO %s", tap->todo_msg);
+		if(!ok) tap->failed_tests--;
 	}
-
 	printf("\n");
-
-	if(!ok) {
-		diag("    Failed %stest (%s:%s() at line %d)",
-			todo ? "(TODO) " : "", file, func, line);
-	}
-
+	/* detail */
+	if(!ok) diag("    Failed %stest (%s:%s() at line %d)", tap->todo ? "(TODO) " : "", file, func, line);
 	return ok ? 1 : 0;
 }
 
-
-void plan_no_plan(void)
-{
-	TRY_INIT;
-	if(have_plan) {
-		fprintf(stderr, "You tried to plan twice!\n");
-		test_died = true;
-		abort();
-	}
-
-	have_plan = true;
-	no_plan = true;
+static void plan_once(void) {
+	if(!tap->have_plan) return;
+	fprintf(stderr, "You tried to plan twice!\n");
+	tap->test_died = true;
+	abort();
 }
 
+void plan_no_plan(void) {
+	plan_once();
+	tap->have_plan = true; tap->no_plan = true;
+}
 
 static void plan_skip_all_v(const char *fmt, va_list al)
 {
-	TRY_INIT;
-	if(have_plan) {
-		fprintf(stderr, "You tried to plan twice!\n");
-		test_died = true;
-		abort();
-	}
-
-	have_plan = true;
-	skip_all = true;
-
+	plan_once();
+	tap->have_plan = true; tap->skip_all = true;
 	print_sub_prefix();
 	printf("1..0");
 	if(fmt != NULL) {
@@ -339,43 +208,28 @@ static void plan_skip_all_v(const char *fmt, va_list al)
 		vfprintf(stdout, fmt, al);
 	}
 	printf("\n");
-
-	/* FIXME: do a longjmp-equivalent to the top level. or exit the thread. or
-	 * something.
-	 */
+	/* TODO: nonlocal exit to the top level, for comfort? */
 }
 
-
-void plan_skip_all(const char *fmt, ...)
-{
-	va_list al;
-	va_start(al, fmt);
+void plan_skip_all(const char *fmt, ...) {
+	va_list al; va_start(al, fmt);
 	plan_skip_all_v(fmt, al);
 	va_end(al);
 }
 
-
 void plan_tests(unsigned int num_tests)
 {
-	TRY_INIT;
-	if(have_plan) {
-		fprintf(stderr, "You tried to plan twice!\n");
-		test_died = true;
-		abort();
-	}
-
+	plan_once();
 	if(num_tests == 0) {
 		fprintf(stderr, "You said to run 0 tests! You've got to run something.\n");
-		test_died = true;
+		tap->test_died = true;
 		abort();
 	}
-
-	have_plan = 1;
+	tap->have_plan = 1;
 	print_sub_prefix();
 	printf("1..%u\n", num_tests);
-	expected_tests = num_tests;
+	tap->expected_tests = num_tests;
 }
-
 
 void planf(int tests, const char *fmt, ...)
 {
@@ -383,108 +237,84 @@ void planf(int tests, const char *fmt, ...)
 	else if(tests == NO_PLAN) plan_no_plan();
 	else {
 		assert(tests == SKIP_ALL);
-		va_list al;
-		va_start(al, fmt);
+		va_list al; va_start(al, fmt);
 		plan_skip_all_v(fmt, al);
 		va_end(al);
 	}
 }
 
-
 int diag(const char *fmt, ...)
 {
-	va_list al;
-	va_start(al, fmt);
-	char msg[512];
-	vsnprintf(msg, sizeof(msg), fmt, al);
+	va_list al; va_start(al, fmt);
+	char msg[200]; vsnprintf(msg, sizeof msg, fmt, al);
 	va_end(al);
-
 	char *s = msg, *lf;
 	do {
-		lf = strchr(s, '\n');
-		if(lf != NULL) *lf = '\0';
+		if(lf = strchr(s, '\n'), lf != NULL) *lf = '\0';
 		print_sub_prefix();
 		fprintf(stderr, "# %s\n", s);
 		if(lf != NULL) s = lf + 1;
 	} while(lf != NULL);
-
 	return 0;
 }
 
-
 int skip(unsigned int num_skip, const char *reason, ...)
 {
-	char msg[512];
-	va_list al;
-	va_start(al, reason);
-	vsnprintf(msg, sizeof(msg), reason, al);
+	va_list al; va_start(al, reason);
+	char msg[200]; vsnprintf(msg, sizeof msg, reason, al);
 	va_end(al);
 	for(unsigned int i = 0; i < num_skip; i++) {
-		num_tests_run++;
+		tap->num_tests_run++;
 		print_sub_prefix();
-		printf("ok %d # skip %s\n", num_tests_run, msg);
+		printf("ok %d # skip %s\n", tap->num_tests_run, msg);
 	}
-
 	return 1;
 }
 
-
 void todo_start(const char *fmt, ...)
 {
-	char msg[512];
-	va_list al;
-	va_start(al, fmt);
-	int len = vsnprintf(msg, sizeof(msg), fmt, al);
+	va_list al; va_start(al, fmt);
+	char msg[200]; int len = vsnprintf(msg, sizeof msg, fmt, al);
 	va_end(al);
-
-	/* FIXME: this may leave todo_msg NULL. it should be replaced with a
+	/* TODO: this may leave todo_msg NULL. it should be replaced with a
 	 * "tap.c malloc() issue" type message, as in libtap.
 	 */
-	free(todo_msg);
-	todo_msg = malloc(len + 1);
-	if(todo_msg != NULL) {
-		memcpy(todo_msg, msg, len + 1);
-	}
-
-	todo = true;
+	free(tap->todo_msg);
+	if(tap->todo_msg = malloc(len + 1), tap->todo_msg != NULL) memcpy(tap->todo_msg, msg, len + 1);
+	tap->todo = true;
 }
 
-
-void todo_end(void)
-{
-	todo = false;
-	free(todo_msg);
-	todo_msg = NULL;
+void todo_end(void) {
+	tap->todo = false;
+	free(tap->todo_msg); tap->todo_msg = NULL;
 }
-
 
 int exit_status(void)
 {
-	if(no_plan || !have_plan) return failed_tests;
-	else if(expected_tests < num_tests_run) {
+	if(tap->no_plan || !tap->have_plan) return tap->failed_tests;
+	else if(tap->expected_tests < tap->num_tests_run) {
 		/* ran too many. return number of unplanned tests */
-		return num_tests_run - expected_tests;
+		return tap->num_tests_run - tap->expected_tests;
 	} else {
 		/* failed + didn't run */
-		return failed_tests + expected_tests - num_tests_run;
+		return tap->failed_tests + tap->expected_tests - tap->num_tests_run;
 	}
 }
 
-
 void close_no_plan(void)
 {
-	if(no_plan_closed) return;
-	if(!have_plan || no_plan) {
+	if(tap->no_plan_closed) return;
+	if(!tap->have_plan || tap->no_plan) {
 		print_sub_prefix();
-		printf("1..%d\n", num_tests_run);
-		expected_tests = num_tests_run;
-		if(!have_plan) {
+		printf("1..%d\n", tap->num_tests_run);
+		tap->expected_tests = tap->num_tests_run;
+		if(!tap->have_plan) {
 			/* discourage an implicit lazy plan */
 			diag("    No plan until end of test?");
-			have_plan = true;	/* shouldn't plan again. */
+			tap->have_plan = true;	/* shouldn't plan again. */
 		}
 	}
-	no_plan_closed = true;
+	tap->no_plan_closed = true;
 }
 
 #endif
