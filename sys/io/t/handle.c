d1d4672ff28332ee92a4a16e2c94b408a1fb0608
Kalle A. Sandström <ksandstr@iki.fi>
2022-06-12 16:31:07 +0300

    sys/io: test IO/dup_to wildcard mode
    This also cleans up and refactors sys/test/spawn-partner.c to better support
    userspace collaboration needs of this test, and more to come.

---
+++ sys/io/t/handle.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <threads.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <l4/types.h>
+#include <l4/ipc.h>
+#include <l4/thread.h>
+#include <sneks/api/io-defs.h>
+#include <sneks/systask.h>
+#include <sneks/test.h>
+
+static const char *testfile = "/initrd/systest/sys/test/initrd_spawn_partner";
+
+struct dup_param {
+	L4_ThreadId_t server;
+	int handle;
+};
+
+static int partner_fn(void *priv)
+{
+	struct dup_param *p = priv;
+	int rc = __io_touch(p->server, p->handle);
+	__io_close(p->server, p->handle);
+	free(p);
+	return rc;
+}
+
+START_LOOP_TEST(dup_to_wildcard, iter, 0, 3)
+{
+	const bool wildcard = iter & 1, systask = iter & 2;
+	diag("wildcard=%s, systask=%s", btos(wildcard), btos(systask));
+	plan_tests(5);
+	todo_start("not done yet");
+	int fd = open(testfile, O_RDONLY);
+	fail_if(fd < 0, "open testfile: %s", strerror(errno));
+	int copy, n = __io_dup_to(fserv(fd), &copy, fhand(fd), wildcard ? 0x10000 : SNEKS_MIN_SYSID);
+	skip_start(!ok(n == 0, "IO/dup"), 4, "failed: %s", stripcerr(n)) {
+		L4_Word_t st;
+		if(!systask) {
+			char server[32], handle[32], self[32];
+			snprintf(server, sizeof server, "SERVER=%#lx", fserv(fd).raw);
+			snprintf(handle, sizeof handle, "HANDLE=%lu", (unsigned long)copy);
+			snprintf(self, sizeof self, "RETURN=%#lx", L4_MyGlobalId().raw);
+			char *argv[] = { "initrd_spawn_partner", "dup-to-wildcard", NULL }, *envp[] = { server, handle, self, NULL };
+			int cpid = spawn_NP("/initrd/systest/sys/test/initrd_spawn_partner", argv, envp);
+			if(!ok(cpid > 0, "spawn")) diag("spawn failed: %s", strerror(errno));
+			skip(1, "userspace partner");
+			L4_ThreadId_t sender;
+			L4_MsgTag_t tag;
+			do tag = L4_Wait(&sender); while(L4_IpcSucceeded(tag) && pidof_NP(sender) != cpid);
+			if(L4_IpcFailed(tag)) st = L4_ErrorCode(); else L4_StoreMR(1, &st);
+		} else {
+			skip(1, "systask thread partner");
+			struct dup_param *param = malloc(sizeof *param); fail_if(param == NULL);
+			*param = (struct dup_param){ .server = fserv(fd), .handle = copy };
+			thrd_t oth; ok(thrd_create(&oth, &partner_fn, param) == thrd_success, "thrd_create");
+			int tmp; thrd_join(oth, &tmp); st = tmp;
+		}
+		if(st != 0) diag("partner status: %s", stripcerr(st));
+		iff_ok1(systask && wildcard, st == 0);
+		imply_ok1(!wildcard || !systask, st == -EBADF);
+	} skip_end;
+	__io_close(fserv(fd), copy);
+	close(fd);
+}
+END_TEST
+
+SYSTEST("io:handle", dup_to_wildcard);
--- sys/test/spawn-partner.c
+++ sys/test/spawn-partner.c
@@ -1,4 +1,4 @@
-
+/* userspace collaborator program for systemspace tests. */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,26 +6,55 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
+#include <ccan/array_size/array_size.h>
+#include <ccan/str/str.h>
 #include <l4/types.h>
 #include <l4/ipc.h>
+#include <sneks/api/io-defs.h>
 
+static unsigned long envul(const char *name) {
+	char *s = getenv(name);
+	if(s == NULL) exit(1);
+	return strtoull(s, NULL, 0);
+}
+
+static int send_status(int n) {
+	L4_ThreadId_t dest = { .raw = envul("RETURN") };
+	L4_LoadMR(0, (L4_MsgTag_t){ .X.u = 1, .X.label = 0xfddb }.raw);
+	L4_LoadMR(1, n);
+	return L4_IpcSucceeded(L4_Send(dest)) ? 0 : L4_ErrorCode();
+}
+
+static int fork_and_exit(void) /* from uapi:lifecycle */
+{
+	int child = fork();
+	if(child < 0) {
+		perror("fork");
+		return 1;
+	} else if(child == 0) {
+		exit(666);
+	} else {
+		int st, n = waitpid(child, &st, 0);
+		if(n < 0) perror("waitpid");
+		return child;
+	}
+}
+
+static int dup_to_wildcard(void) /* from io:handle */
+{
+	L4_ThreadId_t server = { .raw = envul("SERVER") };
+	int handle = envul("HANDLE");
+	return send_status(__io_touch(server, handle));
+}
 
 int main(int argc, char *argv[])
 {
-	/* mode of uapi:lifecycle:notify_fork_exit */
-	if(argc > 1 && strcmp(argv[1], "fork-and-exit") == 0) {
-		int child = fork();
-		if(child < 0) {
-			fprintf(stderr, "fork failed, errno=%d\n", errno);
-			return 1;
-		} else if(child == 0) {
-			exit(666);
-		} else {
-			int st, n = waitpid(child, &st, 0);
-			if(n < 0) fprintf(stderr, "waitpid failed, errno=%d\n", errno);
-			return child;
-		}
+	static const struct { const char *name; int (*fn)(void); } modes[] = {
+		{ "fork-and-exit", &fork_and_exit },
+		{ "dup-to-wildcard", &dup_to_wildcard },
+	};
+	for(int i=0; i < ARRAY_SIZE(modes) && argc > 1; i++) {
+		if(streq(modes[i].name, argv[1])) return (*modes[i].fn)();
 	}
 
 	L4_ThreadId_t oth = { .raw = strtoul(argv[1], NULL, 0) };
